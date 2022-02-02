6372790dd172e15f31074a82c017d4a97e5cf809
JebemtiMaiku <niki.iff@hotmail.com>
2022-02-02 21:04:33 +0100

    0.1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# jpegvision
+# noindex
+python script for destroying .avi files
+
+1. put video in the same folder as file
+2. open terminal
+3. navigate to the folder with script and video (cd)
+4. run 'python3 jpegvision.py'
+
+
--- jpegvision.py
+++ jpegvision.py
@@ -2,6 +2,41 @@ import os, glob, cv2, shutil, ffmpeg
 import numpy as np
 from PIL import Image
 
+print (" ")
+print (" ________________________________________________________")
+print ("|        _                           _      _            |")
+print ("|       (_)___  ___  ____ _   _   __(_)____(_)___  ____  |")
+print ("|      / / __ \/ _ \/ __ `/  | | / / / ___/ / __ \/ __ \ |")
+print ("|     / / /_/ /  __/ /_/ /   | |/ / (__  ) / /_/ / / / / |")
+print ("|  __/ / .___/\___/\__, /    |___/_/____/_/\____/_/ /_/  |")
+print ("| /___/_/         /____/                                 |")
+print ("|________________________________________________________|")
+print ("|                                                        |")
+print ("| jpegvision.py v0.1 2022                                |")
+print ("| \ \ jpeg compression video filter                      |")
+print ("|                                                        |")
+print ("|________________________________________________________|")
+print ("|                                                        |")
+print ("| dev@niklausiff.ch                                      |")
+print ("| https://www.niklausiff.rip                             |")
+print ("|________________________________________________________|")
+print (" ")
+
+print (">>> 0/3 settings")
+print ("__________________________________________________________")
+
+
+
+# input settings
+print (" ")
+print ("motion blur amount")
+ksizef = input("> value: ")
+
+print ("__________________________________________________________")
+print (" ")
+print (">>> 1/3 create folders")
+print ("__________________________________________________________")
+
 # create new folders / delete if already exists
 dir = './'
 directoryog = 'og'
@@ -31,7 +66,7 @@ os.mkdir(newpatho)
 
 # motion blur functions
 def mb_vert(img):
-    kernel_size = ksize 
+    kernel_size = int(ksize) 
     kernel_v = np.zeros((kernel_size, kernel_size))
     kernel_v[:, int((kernel_size - 1)/2)] = np.ones(kernel_size)
     kernel_v /= kernel_size
@@ -48,8 +83,8 @@ def mb_hori(img):
 # noise function
 def noise(img):
     row,col,ch= img.shape
-    mean = 10
-    var = 200
+    mean = 20
+    var = 400
     sigma = var**0.5
     gauss = np.random.normal(mean,sigma,(row,col,ch))
     gauss = gauss.reshape(row,col,ch)
@@ -71,6 +106,10 @@ def img_resize(img, width = None, height = None, inter = cv2.INTER_AREA):
     resized = cv2.resize(img, dim, interpolation = inter)
     return resized
 
+print (" ")
+print (">>> 2/3 apply jpeg vision filter")
+print ("__________________________________________________________")
+
 # get movie
 getVideo = []
 for filename in os.listdir(dir):
@@ -82,46 +121,47 @@ for filename in os.listdir(dir):
     ffmpeg
     .input(getVideo[0])
     .filter('fps', fps='25')
-    .output('og/%03d.jpg', start_number=0)
+    .output('og/%09d.jpg', start_number=0)
     .overwrite_output()
     .run(quiet=True)
 )
 
 # get list of jpges
 pathog = newpathog+'/'
-jpg_list = glob.glob(pathog + "*.jpg")
+jpg_list = sorted(glob.glob(pathog + "*.jpg"))
 
 # change reso / add motion blur / add noise
 pathf = newpathf+'/'
 ext = '.jpg'
-ksize = 100
+# ksize = 100
+ksize = ksizef
 for x in range(len(jpg_list)):
     img = cv2.imread(jpg_list[x])
     img = img_resize(img, width = 1280)
     img = mb_vert(img)
     img = noise(img)
-    cv2.imwrite(pathf+str(x)+ext, img, [int(cv2.IMWRITE_JPEG_QUALITY), 20])
+    cv2.imwrite(pathf+"{0:09}".format(x)+ext, img, [int(cv2.IMWRITE_JPEG_QUALITY), 20])
 
 # change reso / render again
 paths = newpaths+'/'
 ext = '.jpg'
 ksize = 20
-jpg_list_s = glob.glob(pathf + "*.jpg")
+jpg_list_s = sorted(glob.glob(pathf + "*.jpg"))
 for x in range(len(jpg_list_s)):
     img = cv2.imread(jpg_list_s[x])
     img = img_resize(img, width = 860)
     img = mb_vert(img)
-    cv2.imwrite(paths+str(x)+ext, img, [int(cv2.IMWRITE_JPEG_QUALITY), 60])
+    cv2.imwrite(paths+"{0:09}".format(x)+ext, img, [int(cv2.IMWRITE_JPEG_QUALITY), 60])
 
 # scale up / render again
 patho = newpatho+'/'
 ext = '.jpg'
 ksize = 20
-jpg_list_o = glob.glob(paths + "*.jpg")
+jpg_list_o = sorted(glob.glob(paths + "*.jpg"))
 for x in range(len(jpg_list_o)):
     img = cv2.imread(jpg_list_o[x])
-    img = img_resize(img, width = 2000, height = 2000)
-    cv2.imwrite(patho+str(x)+ext, img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    img = cv2.resize(img, (2000,2000), interpolation = cv2.INTER_AREA)
+    cv2.imwrite(patho+"{0:09}".format(x)+ext, img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
 # create new video
 (
@@ -130,3 +170,29 @@ for x in range(len(jpg_list_o)):
     .output('jpegvision.mp4')
     .run(quiet=True)
 )
+
+print (" ")
+print (">>> 3/3 delete not needed folders")
+print ("__________________________________________________________")
+
+# delete folders
+shutil.rmtree(pathog)
+shutil.rmtree(pathf)
+shutil.rmtree(paths)
+shutil.rmtree(patho)
+
+print (" ")
+print (" ________________________________________________________ ")
+print ("|                                                        |")
+print ("| finished                                               |")
+print ("|________________________________________________________|")
+print ("|                                                        |")
+print ("| processed video:                                       |")
+print ("| ./jpegvision.mp4                                       |")
+print ("|                                                        |")
+print ("|________________________________________________________|")
+print ("|                                                        |")
+print ("| dev@niklausiff.ch                                      |")
+print ("| https://www.niklausiff.ch                              |")
+print ("|________________________________________________________|")
+print (" ")
